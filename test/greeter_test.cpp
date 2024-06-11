c2f7da7e339ddfd2fa0201c0c7882c9ee0ffb26c
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2024-06-11 15:10:27 +0200

    Fix test

--- test/greeter_test.cpp
+++ test/greeter_test.cpp
@@ -5,5 +5,5 @@
 TEST(GreeterTest, GreeterTest) {
     // This test is a placeholder
     // It will be replaced by a real test
-    EXPECT_EQ(Greeter::greet("World"), std::string("Hello, World!"));
+    EXPECT_EQ(Greeter::greet("World"), std::string("Hello, World!\n"));
 }
